24ad2bd98f0bf8bbb34fdb170b70c34cc98e2cd6
Daniel Rivera <daniele.riverag@konradlorenz.edu.co>
2018-04-17 18:13:38 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/konradlorenz/quiz/Maintwo.java
@@ -0,0 +1,13 @@
+package co.edu.konradlorenz.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Maintwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintwo);
+    }
+}
